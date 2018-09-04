373a15fae5b88a10fa365a967ca75d9f2317f9d8
udam.arosha@gmail.com <922071543aro>
2018-09-04 19:27:44 +0530

    first

---
+++ WebAppAro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppAro.MvcApplication" Language="C#" %>
---
+++ WebAppAro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAro.Startup))]
+namespace WebAppAro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
